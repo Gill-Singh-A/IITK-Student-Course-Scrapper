252ce57800c3090acdbdf96bff9c7cf5e7056af9
Gill-Singh-A <amansg22@iitk.ac.in>
2024-07-02 03:49:28 +0530

    Implemented Multithreading

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests, pickle
 from datetime import date
 from bs4 import BeautifulSoup
 from optparse import OptionParser
+from multiprocessing import Pool, cpu_count, Lock
 from time import strftime, localtime
 from colorama import Fore, Back, Style
 
@@ -22,6 +23,9 @@ tag_id = {0: "Academic Session",
           4: "Instructor",
           5: "Department"}
 
+thread_count = cpu_count()
+lock = Lock()
+
 def display(status, data, start='', end='\n'):
     print(f"{start}{status_color[status]}[{status}] {Fore.BLUE}[{date.today()} {strftime('%H:%M:%S', localtime())}] {status_color[status]}{Style.BRIGHT}{data}{Fore.RESET}{Style.RESET_ALL}", end=end)
 
@@ -31,6 +35,29 @@ def get_arguments(*args):
         parser.add_option(arg[0], arg[1], dest=arg[2], help=arg[3])
     return parser.parse_args()[0]
 
+def getStudentsCourseDetails(thread_index, student_data):
+    courseDetails = {}
+    for index, roll in enumerate(student_data):
+        course_details = []
+        response = requests.get(f"http://172.26.142.68/dccourse/studdc.php?roll_no={roll}")
+        page = BeautifulSoup(response.text, "html.parser")
+        tr_tags = page.find_all("tr")[1:]
+        for tr_tag in tr_tags:
+            course_detail = {}
+            td_tags = tr_tag.find_all("td")
+            tag_range = len(tag_id) if len(tag_id) < len(td_tags) else len(td_tags)
+            try:
+                for tag_index in range(tag_range):
+                    course_detail[tag_id[tag_index]] = td_tags[tag_index].text.strip()
+            except:
+                with lock:
+                    display('-', f"Error while Scrapping the Data of {Back.MAGENTA}{roll}{Back.RESET}", start='\r')
+                continue
+            course_details.append(course_detail)
+        courseDetails[roll] = course_details
+        with lock:
+            display('*', f"Entity Scrapped = {Back.MAGENTA}{index+1}/{len(student_data)} ({(index+1)/len(student_data)*100:.2f}%){Back.RESET}", start=f"{thread_index+1} ")
+    return courseDetails
 if __name__ == "__main__":
     data = get_arguments(('-l', "--load", "load", "File of Roll Numbers to load (pickle file)"),
                          ('-w', "--write", "write", "Name of the File for the data to be dumped (default=current data and time)"))
@@ -52,24 +79,16 @@ if __name__ == "__main__":
     if not data.write:
         data.write = f"{date.today()} {strftime('%H_%M_%S', localtime())}"
     courseDetails = {}
-    for index, roll in enumerate(student_data):
-        course_details = []
-        response = requests.get(f"http://172.26.142.68/dccourse/studdc.php?roll_no={roll}")
-        page = BeautifulSoup(response.text, "html.parser")
-        tr_tags = page.find_all("tr")[1:]
-        for tr_tag in tr_tags:
-            course_detail = {}
-            td_tags = tr_tag.find_all("td")
-            tag_range = len(tag_id) if len(tag_id) < len(td_tags) else len(td_tags)
-            try:
-                for tag_index in range(tag_range):
-                    course_detail[tag_id[tag_index]] = td_tags[tag_index].text.strip()
-            except:
-                display('-', f"Error while Scrapping the Data of {Back.MAGENTA}{roll}{Back.RESET}", start='\r')
-                continue
-            course_details.append(course_detail)
-        courseDetails[roll] = course_details
-        display('*', f"Entities Scrapped = {Back.MAGENTA}{index+1}/{len(student_data)} ({(index+1)/len(student_data)*100:.2f}%){Back.RESET}", start='\r', end='')
+    pool = Pool(thread_count)
+    total_students = len(student_data)
+    student_data_divisions = [student_data[group*total_students//thread_count : (group+1)*total_students//thread_count] for group in range(thread_count)]
+    threads = []
+    for index, studetn_data_division in enumerate(student_data_divisions):
+        threads.append(pool.apply_async(getStudentsCourseDetails, (index, studetn_data_division, )))
+    for thread in threads:
+        thread.get()
+    pool.close()
+    pool.join()
     display(':', f"Dumping Data to {Back.MAGENTA}{data.write}{Back.RESET}", start='\n')
     with open(f"{data.write}", 'wb') as file:
         pickle.dump(courseDetails, file)
